e55788e7f4bcdffa42eeccf7cdcb7136f0effdaa
Arai Duisebekova <arai.dsb1@gmail.com>
2023-10-01 12:41:20 +0600

    feat(DRC-1): Added features

--- stone-paper-scissors/ViewController.swift
+++ stone-paper-scissors/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var versusImage: UIImageView!
+    @IBOutlet weak var playButton: UIButton!
+    
+    let imagesArray = [
+        UIImage(named: "paper"),
+        UIImage(named: "scissors"),
+        UIImage(named: "rock")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func playButtonPressed(_ sender: UIButton) {
+        imageView1.image = imagesArray[randomImage()]
+        imageView2.image = imagesArray[randomImage()]
+    }
+    
+    func randomImage() -> Int {
+        return Int.random(in: 0..<imagesArray.count)
+    }
 }
 
